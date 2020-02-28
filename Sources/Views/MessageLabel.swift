f63ac6065cf4394107178067f31453c8a711d313
Shohei Yoshikawa <shohei@fs276edb41.tkyc512.ap.nuro.jp>
2020-02-28 19:13:37 +0900

    check if can open url before

--- Sources/Views/MessageLabel.swift
+++ Sources/Views/MessageLabel.swift
@@ -479,7 +479,7 @@ open class MessageLabel: UILabel {
             guard let date = date else { return }
             handleDate(date)
         case let .link(url):
-            guard let url = url else { return }
+            guard let url = url, UIApplication.shared.canOpenURL(url) else { return }
             handleURL(url)
         case let .transitInfoComponents(transitInformation):
             var transformedTransitInformation = [String: String]()
